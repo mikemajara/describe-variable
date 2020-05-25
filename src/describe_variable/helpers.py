0976cd5d5a9c87cd94b5b4664622ba25d02fcf36
mikemajara <fmiguel05@gmail.com>
2020-05-25 18:08:50 +0200

    Fix types in empty lists

--- src/describe_variable/helpers.py
+++ src/describe_variable/helpers.py
@@ -4,14 +4,20 @@ from typing import Union
 def type_of_components(obj: Union[dict, list]) -> str:
     if isinstance(obj, list):
         indices = list(range(0, len(obj)))
-        first_type = type(obj[indices[0]])
+        if len(indices) <= 0:
+            first_type = None
+        else:
+            first_type = type(obj[indices[0]])
     elif isinstance(obj, dict):
         indices = list(obj.keys())
-        first_type = type(obj[indices[0]])
+        if len(indices) <= 0:
+            first_type = None
+        else:
+            first_type = type(obj[indices[0]])
     else:
         raise TypeError(str(obj) + " must be a list or a dict")
 
     if all(isinstance(obj[idx], first_type) for idx in indices):
-        return first_type.__name__
+        return first_type.__name__ if first_type is not None else 'empty'
     else:
         return 'mixed'
